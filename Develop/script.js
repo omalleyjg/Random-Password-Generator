58f43277b8f19b39bfa9e3bc8f386961dff33537
omalleyjg <omalley.jg@gmail.com>
2023-01-16 07:29:38 -0400

    added criteria for password generation. further testing needed.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Password object
+
+var passwordCriteria = {
+
+  passwordLength: 0,
+
+  lowerCase: [abcdefghijklmnopqrstuvwxyz],
+
+  upperCase: [ABCDEFGHIJKLMNOPQRSTUVWXYZ],
+
+  numbers: [1234567890],
+
+  symbols: [!@#$%&*?],
+
+  
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +30,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
