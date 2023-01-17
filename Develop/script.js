47abc05db2026cdaf88bb08d9ef29f6441223e66
omalleyjg <omalley.jg@gmail.com>
2023-01-17 19:11:45 -0400

    added working prompts, still does not generate password, returns undefined.

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,63 @@
+var passwordLength = 8;
+var choiceArr = [];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var symbols = ['!', '@', '#', '$', '%', '&', '*', '?'];
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
   console.log("Button pressed.");
-
-  //1. prompt the user for password length 8 < 128
-  
-  //2. validate length of password based on user input
-  
-  //3. prompt the user for uppercase letters
-  
-  //4. prompt the user for lowercase letters
-  
-  //5. prompt the user for numbers
-  
-  //6. prompt the user for symbols
-  
-  //7. generte random password based on criteria
-  
-  //8. return passowrd to page
-  return ("Password will go here.")
+  var password = "";
+  for (i = 0; i < passwordLength; i++);
 }
+
+function getPrompts() {
+
+  choiceArr = [];
+
+  passwordLength = parseInt(prompt("How long would you like your password to be? Must be between 8 and 128 characters."));
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+  alert("Must be a number between 8 and 128. Please try again.");
+  return false;
+}
+if (confirm("Would you like lowercase letters?")) {
+  choiceArr = choiceArr.concat(lowerCase);
+}
+if (confirm("Would you like uppercase letters?")) {
+  choiceArr = choiceArr.concat(upperCase);
+}
+if (confirm("Would you like numbers letters?")) {
+  choiceArr = choiceArr.concat(numbers);
+}
+if (confirm("Would you like symbols letters?")) {
+  choiceArr = choiceArr.concat(symbols);
+}
+return true;
+}
+
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var prompts = getPrompts();
 
+  if (prompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
